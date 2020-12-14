5d70610a4fa1c1c8611b1468e6b8f439c42e4639
anthonyeca <anthonyeca19esp@gmail.com>
2020-12-14 14:57:08 +0100

    upgrade

--- app.js
+++ app.js
@@ -3,3 +3,24 @@ const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 tl.to('.text',{y:'0%', duration:1, stagger: 0.25});
 tl.to('.intro',{y:"-100%", duration:1});
 tl.fromTo('header',{opacity:0},{opacity:1, duration:2});
+/*
+var gamer = [103,097,109,101,1144].join(),
+    $div = $('div');
+
+$(window).on('keypress', function(e)){
+    var $this = $(this),
+        gap = e.timeStamp - ($this.data('time') || e.timeStamp),
+        chrs = $this.data('chrs') || [];
+
+    if (gap > 1000){
+        chars =[];
+    }
+
+    chars.push(e.wich);
+    if(chars.join()===gamer){
+        window.open("https://google.com",'_blank');
+    }
+
+    this.data('chars',chars);
+    $this.data('time',e.timeStamp);
+}
